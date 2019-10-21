d4f72ca8f2bd194ba8696fb9740e822ad7442b7b
LeoCR <leonardoas2007@gmail.com>
2019-10-20 19:35:26 -0600

    Removed __pycache__

--- django_app/views.py
+++ django_app/views.py
@@ -3,4 +3,4 @@ from django.http import HttpResponse
 # Create your views here.
 def index(request):
     temp_dict={'django_variable':"I am a Django Variable"}
-    return render(request,'index.html',context=temp_dict)
+    return render(request,'app/index.html',context=temp_dict)
