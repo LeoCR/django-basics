6b258eeafa0dba6b4e57f636ab5a26145ef00aa8
LeoCR <leonardoas2007@gmail.com>
2019-10-20 19:30:52 -0600

    Injecting variables in html templates

---
+++ django_app/views.py
@@ -0,0 +1,6 @@
+from django.shortcuts import render
+from django.http import HttpResponse
+# Create your views here.
+def index(request):
+    temp_dict={'django_variable':"I am a Django Variable"}
+    return render(request,'index.html',context=temp_dict)
