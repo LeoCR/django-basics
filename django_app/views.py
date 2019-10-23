221d95834349b63b46cdf2cd7177a2cce2043112
LeoCR <leonardoas2007@gmail.com>
2019-10-23 11:49:49 -0600

    Implementing Django Forms inside templates

--- django_app/views.py
+++ django_app/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 from django_app.models import Topic,Webpage,AccessRecord
+from . import forms
 # Create your views here.
 def index(request):
     webpages_list = AccessRecord.objects.order_by('date')
@@ -8,7 +9,18 @@ def index(request):
         'access_records':webpages_list
     }
     return render(request,'app/index.html',context=date_dict)
+def form_name_view(request):
+    form = forms.FormName()
+    if request.method == 'POST':
+        form = forms.FormName(request.POST)
 
+        if form.is_valid():
+            print("Validation Success")
+            print("Name: "+form.cleaned_data['name'])
+            print("Email: "+form.cleaned_data['email'])
+            print("Text: "+form.cleaned_data['text'])
+
+    return render(request,'app/form_page.html',{'form':form})
 def content(request):
     temp_dict={
         'content':"Hello I am a Django Content"
